29c1a21a58538f29739b3c9c04177087ebce0365
kate.po <ketringggold@mail.ru>
2025-01-27 20:40:04 +0300

    добавлено решение заданий

--- lesson4/list_5.py
+++ lesson4/list_5.py
@@ -5,3 +5,9 @@
 Добавить элемент на 2 место 'indesit'
 
 '''
+brands = ['samsung', 'lg', 'xerox', 'bosch']
+
+brands.remove('xerox')
+brands.insert(1, 'indesit')
+
+print("Отредактированный список: ", brands)
