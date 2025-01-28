bdaacd884e8c97ae24ef5086662c869a59c31eb1
kate.po <ketringggold@mail.ru>
2025-01-28 17:47:29 +0300

    испралена ошибка с переменными

--- lesson4/str_2.py
+++ lesson4/str_2.py
@@ -9,10 +9,10 @@
 nums = input("Введите цифры через пробел: ")
 
 new_list = nums.split()
-numbers = list(map(int,a))
+numbers = list(map(int,new_list))
 s = sum(numbers)
 m_num = max(numbers)
 lenth_1 = len(numbers)
-average = s / c
+average = s / lenth_1
 print(f"Общая сумма цифр: {s} \nМаксимальная цифра из списка: {m_num} \nСреднее арифметическое: {average}")
 
