5a630a68bbe62df359bb758ceb19e3b3498a82bc
kate.po <ketringggold@mail.ru>
2025-02-25 15:16:27 +0000

    ready one task

--- lesson12/1.py
+++ lesson12/1.py
@@ -18,3 +18,35 @@
 Модель: {}
 Год выпуска: {}
 """
+
+class Phone:
+        def __init__(self, brand, model, issue_year):
+                self.brand = brand
+                self.model = model
+                self.issue_year = issue_year
+
+        def receive_call(self, name):
+                print(f"{self.brand}-{self.model} - Звонит {name}")
+
+        def get_info(self):
+                return (self.brand, self.model, self.issue_year)
+                
+        def __str__(self):
+                return f"Бренд: {self.brand}\nМодель: {self.model}\nГод выпуска:{self.issue_year}"
+
+
+# экземпляры класса Phone
+phone1 = Phone("Samsung", "Galaxy S21", 2021)
+phone2 = Phone("Apple", "iPhone 13", 2022)
+
+# метод __str__
+print(phone1)
+print(phone2)
+
+# receive_call
+phone1.receive_call("Алиса")
+phone2.receive_call("Боб")
+
+# get_info
+print(phone1.get_info())
+print(phone2.get_info())
